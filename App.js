f38aed9c6631bdfdec3b6c95318cbd597fb92395
BURAK EKINCIOGLU - Calisan Deneyimi Cozumleri Mudurlugu <BEKINCIOGLU@THY.COM>
2024-11-26 16:46:21 +0300

    Home screen added

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { createStaticNavigation } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import CarouselScreen from './Screens/CarouselScreen'
+import HomeScreen from './Screens/Home'
 
 const RootStack = createNativeStackNavigator({
   screens: {
+    Home: HomeScreen,
     Carousel: CarouselScreen
   }
 })
--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-const Home = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-      <Button
-      title='Carousel'
-      color={'gray'} 
-      onPress={() => console.log("bune")}
-      />
-    </View>
-  )
+const HomeScreen = () => {
+    const navigation = useNavigation();
+    
+    return (
+        <View style={styles.container}>
+        <Text>Home</Text>
+        <Button
+        title='Carousel'
+        color={'gray'} 
+        onPress={() => navigation.navigate('Carousel')}
+        />
+        </View>
+    )
 }
 
-export default Home
+export default HomeScreen
 
 const styles = StyleSheet.create({
     container: {justifyContent: "center", alignItems: "center"}
