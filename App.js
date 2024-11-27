f6bc1c33fc67e948abea2a503fd911374d123fb1
BURAK EKINCIOGLU - Calisan Deneyimi Cozumleri Mudurlugu <BEKINCIOGLU@THY.COM>
2024-11-27 10:02:03 +0300

    navigation hook içeri alındı

--- App.js
+++ App.js
@@ -3,16 +3,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import CarouselScreen from './Screens/CarouselScreen'
 import HomeScreen from './Screens/Home'
 
-const RootStack = createNativeStackNavigator({
-  screens: {
-    Home: HomeScreen,
-    Carousel: CarouselScreen
-  }
-})
-
-const Navigation = createStaticNavigation(RootStack)
-
 const App = () => {
+  const RootStack = createNativeStackNavigator({
+    screens: {
+      Home: HomeScreen,
+      Carousel: CarouselScreen
+    }
+  })
+
+  const Navigation = createStaticNavigation(RootStack)
+
   return <Navigation />
 }
 
