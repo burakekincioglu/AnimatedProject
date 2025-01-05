06715a8220b0bdd53a9175d3e4417bbf306e5e12
BURAK EKINCIOGLU - Calisan Deneyimi Cozumleri Mudurlugu <BEKINCIOGLU@THY.COM>
2025-01-05 22:12:40 +0300

    pangesture game v2 eklendi

--- Screens/PangestureGameV2.tsx
+++ Screens/PangestureGameV2.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Dimensions, StyleSheet, View } from 'react-native'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler'
-import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated'
+import Animated, { clamp, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated'
 import { CENTER_ITEMS } from '../utils/styles'
 
 const SIZE = 80
-const CIRCLE_RADIOUS = SIZE * 2
 
 interface Context {
     translationX: number
@@ -20,6 +19,7 @@ const PangestureGame = () => {
       const translationY = useSharedValue(0);
       const prevTranslationX = useSharedValue(0);
       const prevTranslationY = useSharedValue(0);
+
       
 
     const pan = Gesture.Pan()
@@ -29,21 +29,26 @@ const PangestureGame = () => {
       prevTranslationY.value = translationY.value;
     })
     .onUpdate((event) => {
-      console.log(translationX.value, translationY.value);
-      
-      const distance = Math.sqrt(translationX.value ** 2 + translationY.value ** 2)
-      if (distance > CIRCLE_RADIOUS - SIZE / 2 - 5) {
-        prevTranslationX.value = translationX.value
-        prevTranslationY.value = translationY.value
-      }else{
-        translationX.value = prevTranslationX.value + event.translationX
-        translationY.value = prevTranslationY.value + event.translationY
-      }
-      
+      const maxTranslateX = width / 2 - 50;
+      const maxTranslateY = height / 2 - 50;
+
+      translationX.value = clamp(
+        prevTranslationX.value + event.translationX,
+        -maxTranslateX,
+        maxTranslateX
+      );
+      translationY.value = clamp(
+        prevTranslationY.value + event.translationY,
+        -maxTranslateY,
+        maxTranslateY
+      );
     }).
     onEnd(() => {        
-        translationX.value = withSpring(0)
-        translationY.value =  withSpring(0)
+        if (translationX.value > width / 4 - SIZE) {
+            translationX.value = withSpring(width / 2 - SIZE /2 - 4)
+        }else{
+            translationX.value =  withSpring(- width / 2 + SIZE /2 + 4)
+        }
     })
 
     const rSquareStyle = useAnimatedStyle(() => {
@@ -61,9 +66,7 @@ const PangestureGame = () => {
   return (
     <View style={[styles.container, CENTER_ITEMS]} >
         <GestureDetector gesture={pan}>
-          <View style={[styles.circle, CENTER_ITEMS]} >
             <Animated.View style={[styles.square, rSquareStyle]}/>
-          </View>
         </GestureDetector>
     </View>
   )
@@ -74,20 +77,13 @@ export default PangestureGame
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
     },
     square: {
         width: SIZE,
         height: SIZE,
         backgroundColor: '#C70A0C',
         borderRadius: SIZE
-    },
-    circle: {
-      width: CIRCLE_RADIOUS * 2,
-      height: CIRCLE_RADIOUS * 2,
-      borderRadius: CIRCLE_RADIOUS,
-      borderColor: 'rgba(0,0,256,0.5)',
-      borderWidth: 5
     }
 
 })
